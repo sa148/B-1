d53c377d8338a190c1dfe5ffa0b71c7863af0271
sa148 <s.kurono@outlook.jp>
2022-10-01 14:06:50 +0000

    creade新規投稿

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,24 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new(book_params)
+  end
+
+  def create
+    @book = Book.new(book_params)
+     if @book.save
+       redirect_to books_path
+     else
+       render :new
+     end
   end
 
   def index
+    @books = Book.all
+  end
+
+  def book_params
+    params.require(:book).permit(title, :body)
   end
 
 end
